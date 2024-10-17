2a4425a840d160ce1dfa5426296a7a5f4ab9e262
Kishan Kumar <singhkishan9911@gmail.com>
2024-10-17 10:09:20 +0530

    handle jwt error in middleware

--- Server/middleware/fetchUser.js
+++ Server/middleware/fetchUser.js
@@ -3,14 +3,19 @@ require('dotenv').config();
 const secret = process.env.JWTsecret;
 
 const fetchUser = (req, res, next) => {
-    const authtoken = req.header('token');
-    if(!authtoken || authtoken === null) {
+    try {
+        const authtoken = req.header('token');
+        if(!authtoken || authtoken === null) {
+            return res.status(401).json({'error' : 'Unauthorized request'});
+        }
+    
+        const decoded = jwt.verify(authtoken, secret);
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        console.error(error)
         return res.status(401).json({'error' : 'Unauthorized request'});
     }
-
-    const decoded = jwt.verify(authtoken, secret);
-    req.user = decoded.user;
-    next();
 }
 
 module.exports = fetchUser;
